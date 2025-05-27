13b42653812e5e5f9e02f25e2f73a08388c7d30e
GalloJunior <gallojunior@gmail.com>
2025-05-26 22:31:59 -0300

    Adicionado sliders para filmes e séries por genero na home

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -11,11 +11,31 @@ export class MovieApiService {
 
   baseUrl = 'https://api.themoviedb.org/3';
   options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmNlMzA4MDA5MTM4YzExYmQ1ZWMxNTA4NmY2ODQ4MyIsIm5iZiI6MTYyMzE5OTUzNi41MzksInN1YiI6IjYwYzAwZjMwNWIxMjQwMDA3YTc0N2IyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kEm9KuWOTvqNYEW1r4xFOWQ7g7SbsWupIiMss_L3gvk'
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmNlMzA4MDA5MTM4YzExYmQ1ZWMxNTA4NmY2ODQ4MyIsIm5iZiI6MTYyMzE5OTUzNi41MzksInN1YiI6IjYwYzAwZjMwNWIxMjQwMDA3YTc0N2IyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kEm9KuWOTvqNYEW1r4xFOWQ7g7SbsWupIiMss_L3gvk'
+    }
+  };
+
+  // Dados para o Banner - Midias em destaque da semana
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
   }
-};
+
+  // Filmes em Destaque do Dia
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?language=pt-br`, this.options);
+  }
+
+  // Séries em Destaque do Dia
+  trendingSerieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/tv/day?language=pt-br`, this.options);
+  }
+
+  // Filmes de Ação Populares
+  popularActionMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=28&sort_by=popularity.desc`, this.options);
+  }
 
 }
